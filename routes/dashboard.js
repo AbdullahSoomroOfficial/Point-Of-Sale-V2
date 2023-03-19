90bd07f3545d4db5bda1e202780efd9cf5052dc4
Abdullah Soomro <abdullahsoomro.official.com>
2023-03-20 00:48:44 +0500

    C:/Program Files/Git/dashboard is secured with isLoggedIn middlware

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { isLoggedIn } = require("../middleware");
 // controller
 const dashboardController = require("../controllers/dashboard");
 const dashboardRouter = express.Router();
 
-dashboardRouter.get("/", dashboardController.get);
+dashboardRouter.get("/", isLoggedIn, dashboardController.get);
 module.exports = dashboardRouter;
